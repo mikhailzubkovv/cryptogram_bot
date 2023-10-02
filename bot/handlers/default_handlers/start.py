a56cb14c14223960afc195dc8d24825d241691d7
mikhailzubkovv <zubkov-mikhail@mail.ru>
2023-10-02 19:43:29 +0500

    created bot's structure, added echo-bot, command /start & /help for test, inline keybord for main menu

---
+++ bot/handlers/default_handlers/start.py
@@ -0,0 +1,14 @@
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+import bot.keyboad.kb_main_menu
+from bot.handlers.router_create import router
+
+
+@router.message(Command('start'))
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!", reply_markup=bot.keyboad.kb_main_menu.menu)
---
+++ main.py
@@ -0,0 +1,14 @@
+import logging
+import asyncio
+import sys
+
+# There is an import handlers for messages here. Main code uses them despite being marked 'unused import'
+from bot.handlers.default_handlers import start, help, echo
+
+from bot.loader.loader import start_bot
+from bot.config_data.config import BOT_TOKEN
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(start_bot(TOKEN=BOT_TOKEN))
