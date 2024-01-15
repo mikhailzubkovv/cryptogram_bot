d3219225a8c22056486fa075cb444e1511f9ace2
mikhailzubkovv <zubkov-mikhail@mail.ru>
2024-01-15 20:38:45 +0500

    add function to create or delete TEMP directory when bot is running to temporary save images to be sent to users

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from bot.handlers.custom_handlers import top_coins_cheap, top_coins_expensive, c
 
 from bot.loader.loader import start_bot
 from bot.config_data.config import BOT_TOKEN
-from utils.coinranking_api.path_n_clean import clean_tmp
+from utils.coinranking_api.path_n_clean import clean_tmp, create_tmp_folder
 
 
 if __name__ == '__main__':
+    create_tmp_folder()
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(start_bot(TOKEN=BOT_TOKEN))
     clean_tmp()
+    create_tmp_folder()
